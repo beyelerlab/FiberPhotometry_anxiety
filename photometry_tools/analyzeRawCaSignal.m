68f005e486e632c21eefb89d3dc45b5a659e4246
beyelerlab <beyelerlab@gmail.com>
2025-06-04 15:39:51 +0200

    Avoid bug when np oeaks detected for clean zscore calculation

--- photometry_tools/analyzeRawCaSignal.m
+++ photometry_tools/analyzeRawCaSignal.m
@@ -43,25 +43,27 @@ function [clean_zscore, zscore] = process_zscore(dff)
 
     mean_bl = nanmean(dff);
     std_bl = nanstd(dff);
-    zscore = (dff - mean_bl) / std_bl;
+    zscore = (dff - mean_bl) ./std_bl;
+    clean_zscore = zscore;
     
     nSamples = max(size(dff));
     
     [pks,locs,w,p] = findpeaks(zscore,'MinPeakHeight', 2.58, 'MinPeakProminence', 2);
     dff_clean = dff;
     n_pks = size(pks, 2);
-    for i=1:n_pks
-        x = locs(i);
-        i1 = floor(x - w(i));
-        i2 = floor(x + w(i));
-        i1 = max([i1,1]);
-        i2 = min([i2 nSamples]);
-        dff_clean(i1:i2)= nan(1,i2-i1+1);
+    if n_pks
+        for i=1:n_pks
+            x = locs(i);
+            i1 = floor(x - w(i));
+            i2 = floor(x + w(i));
+            i1 = max([i1,1]);
+            i2 = min([i2 nSamples]);
+            dff_clean(i1:i2)= nan(1,i2-i1+1);
+        end
+        mean_bl = nanmean(dff_clean);
+        std_bl = nanstd(dff_clean);
+        clean_zscore = (dff - mean_bl) / std_bl;
     end
-    
-    mean_bl = nanmean(dff_clean);
-    std_bl = nanstd(dff_clean);
-    clean_zscore = (dff - mean_bl) / std_bl;
 
 end
 
